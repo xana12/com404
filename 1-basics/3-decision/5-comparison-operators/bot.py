b6229134f179eb7bbf4d3b6db74d7775e834f716
xana12 <4mogea69@solent.ac.uk>
2019-10-01 10:41:14 +0000

    finished

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,9 @@
+firstnumber = int(input ("Please enter the first number"))
+secondnumber = int(input ("Please enter a second number"))
+if firstnumber<secondnumber:
+    print("The first numberis the smallest")
+elif firstnumber==secondnumber:
+    print("Both are equal")
+else :
+    print("The second number is the smallest")
+
