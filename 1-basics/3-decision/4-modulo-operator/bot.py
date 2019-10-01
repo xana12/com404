9019f4b7fdbcfcf159611b2c3501a03734e0b50b
xana12 <4mogea69@solent.ac.uk>
2019-10-01 10:22:30 +0000

    Finished

---
+++ 1-basics/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,7 @@
+wholenumber=int(input("Please enter a wolenumber") )
+print("please enter a wholenumber")
+if ( wholenumber % 2) ==0:
+    print("The wholenumber is even") 
+else :
+    print("The wholenumber is odd")  
+
