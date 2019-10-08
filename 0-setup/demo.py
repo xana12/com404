8411bd22683f320adef296429921a046a9366b78
xana12 <4mogea69@solent.ac.uk>
2019-10-08 09:26:16 +0000

    finish

---
+++ 0-setup/demo.py
@@ -0,0 +1,9 @@
+
+count = 1
+
+while count <= 100:
+    print(count)
+    count = count+1
+
+for count in range(1, 101, 1):
+    print(count)
---
+++ 1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -0,0 +1,6 @@
+print("How many cables should i remove?")
+cables=int(input())
+count=0
+while(count<cables):
+    print("remove cables")
+    count+=1
