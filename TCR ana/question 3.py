bbd9981d8c2c4989f8903a1bc556329f3992b9a5
xana12 <4mogea69@solent.ac.uk>
2019-10-28 09:46:34 +0000

    finished

--- TCR ana/question 3.py
+++ TCR ana/question 3.py
@@ -3,7 +3,7 @@ miles = int(input())
 
 print("I will count the miles...")
 while miles > 0:
-    print("I have... miles to go before I reach the base" + str(miles))
+    print("I have", miles,"miles to go before I reach the base" + str(miles))
     miles = miles-1 
 
 print("I have arrived at the secret headquarters of the MIB!")
